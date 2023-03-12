a342b8f229139c0da85cca79f96e9dec6b148a20
Dmitriy Romanyuk <dmitriy.romanyuk1@gmail.com>
2023-03-12 16:03:55 +0300

    sem_9_DZ

---
+++ task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке 
+// от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int NaturalNumbersSum(int m, int n)
+{
+    if(n == m)
+    {
+        return m;
+    }
+    
+    return n + NaturalNumbersSum(m,n-1);  
+}
+Console.Write("Введите минимальное число ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное число ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{NaturalNumbersSum(numberM,numberN)}");
---
+++ task68/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int MethodAckerman(int m,int n)
+{
+    if(m > 0 && n > 0) return MethodAckerman(m - 1,MethodAckerman (m, n - 1));
+    else if(m > 0 && n == 0) return MethodAckerman(m - 1, 1);
+    else return n + 1; 
+}
+    
+Console.Write("Введите число M ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{MethodAckerman(numberM,numberN)}");
